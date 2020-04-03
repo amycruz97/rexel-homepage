8a06601a9152fa508be7ab6dce8ca43232d8db57
amycruz97 <amarachiamaechi4@gmail.com>
2020-04-03 04:30:25 +0100

    more responsive layout

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function toogles() {
 
+    var x = document.getElementById("toogleNav");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
